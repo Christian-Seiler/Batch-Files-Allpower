ed035f75251f024886e93ff051d79e23c9feb573
Christian Seiler <Christian-Seiler@users.noreply.github.com>
2016-07-22 20:39:33 +0200

    Create Script

--- Shotdown.bat
+++ Shotdown.bat
@@ -1,7 +1,10 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1404\cocoasubrtf470
-{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+:: define Log-File Name and Location
+set YEAR=%DATE:~-4%
+set MONTH=%DATE:~-7,2%
+set log=\\srv01\Vorlagen\Logs\Log-%YEAR%-%MONTH%.txt
 
-\f0\fs24 \cf0 shutdown now}
+:: Log User
+echo %DATE% %TIME% - Shutdown: %COMPUTERNAME% User: %USERNAME% > %log%
+
+:: force Shutdown Computer
+shutdown /s /f
