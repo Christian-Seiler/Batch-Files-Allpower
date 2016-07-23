32150ec93e3bec5ed4fe8e38777b7cb2855d67c3
Christian Seiler <Christian-Seiler@users.noreply.github.com>
2016-07-23 17:02:45 +0200

    Update

--- text.bat
+++ text.bat
@@ -1,19 +1,25 @@
+@echo off
 setlocal
 set runState=user
 whoami /groups | findstr /b /c:"Mandatory Label\\High Mandatory Level" > nul && set runState=admin
 whoami /groups | findstr /b /c:"Mandatory Label\\System Mandatory Level" > nul && set runState=system
 echo Running in state: "%runState%"
 if not "%runState%"=="user" goto notUser
-  echo Do user stuff...
-  goto end
+echo Do user stuff...
+goto end
+
 :notUser
 if not "%runState%"=="admin" goto notAdmin
-  echo Do admin stuff...
-  goto end
+echo Do admin stuff...
+goto end
+
 :notAdmin
 if not "%runState%"=="system" goto notSystem
-  echo Do admin stuff...
-  goto end
+echo Do admin stuff...
+goto end
+
 :notSystem
 echo Do common stuff...
+
 :end
+pause
